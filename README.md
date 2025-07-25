ec927c1fc0f3e483fad159d539c4ff798ee63ab0
Shaad <141047163+Shaad98@users.noreply.github.com>
2025-07-25 19:40:30 +0530

    Using Csrf token Post request is done

--- README.md
+++ README.md
@@ -14,3 +14,10 @@
     spring.security.user.password=s@123
 ```
 
+#### NOTE
+1.Above two implementations disadvantage is only 1 username and password you have  
+2.In application file UsernamePaswordAutheticationFilter check you define username and password or not if not it provide to console.
+3.Where you want to do Post,Put,Delete req from there first get csrf (if postman then postman,if browser then browser (It's IMP))
+
+
+
--- demo/src/main/java/com/example/demo/controller/ApiController.java
+++ demo/src/main/java/com/example/demo/controller/ApiController.java
@@ -2,7 +2,41 @@ package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.model.Users;
+import com.example.demo.service.UserService;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+
 @RestController
 public class ApiController {
 
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/notify")
+    public String sendNotify() {
+        return "I will send notification!";
+    }
+
+    @PostMapping("/save")
+    public Users insertUser(@RequestBody Users user) {
+        return userService.save(user);
+    }
+    
+    @GetMapping("/csrf-token")
+    public CsrfToken getCsrfToken(HttpServletRequest request)
+    {
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
+    
 }
